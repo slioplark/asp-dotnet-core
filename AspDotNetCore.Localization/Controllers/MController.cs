20e1cf5c17ee919f98c2dc75ae32707a254f4157
slioplark <jeailfy9520@outlook.com>
2018-04-04 16:19:47 +0800

    add localization in model

---
+++ AspDotNetCore.Localization/Controllers/MController.cs
@@ -0,0 +1,13 @@
+﻿using AspDotNetCore.Localization.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCore.Localization.Controllers
+{
+    public class MController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(model: new LocalizationViewModel());
+        }
+    }
+}
---
+++ AspDotNetCore.Localization/Models/LocalizationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspDotNetCore.Localization.Models
+{
+    public class LocalizationViewModel
+    {
+        [Display(Name = "Language")]
+        public string Language { get; set; }
+        [Display(Name = "Translation")]
+        public string Translation { get; set; }
+        [Display(Name = "One")]
+        public string One { get; set; }
+        [Display(Name = "Two")]
+        public string Two { get; set; }
+        [Display(Name = "Three")]
+        public string Three { get; set; }
+        [Display(Name = "Four")]
+        public string Four { get; set; }
+        [Display(Name = "Five")]
+        public string Five { get; set; }
+        [Display(Name = "Six")]
+        public string Six { get; set; }
+        [Display(Name = "Seven")]
+        public string Seven { get; set; }
+        [Display(Name = "Eight")]
+        public string Eight { get; set; }
+        [Display(Name = "Nine")]
+        public string Nine { get; set; }
+        [Display(Name = "Ten")]
+        public string Ten { get; set; }
+    }
+}
